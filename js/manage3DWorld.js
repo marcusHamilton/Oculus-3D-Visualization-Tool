a4e42406355a487f024d52ffbaf5e41ca1de49c3
unimatrixZero1 <d.eklund.mx3@gmail.com>
2018-01-23 02:24:29 -0600

    Datapoints are now colored based on XYZ magnitude. Camera orbit controls implemented. randomNumberCSVGen.py now outputs float values.

--- js/manage3DWorld.js
+++ js/manage3DWorld.js
@@ -1,7 +1,6 @@
 /*
   This js file is responsible for building the 3D world
 */
-
 //Set up essential global elemets
 var scene;
 var camera;
@@ -11,7 +10,16 @@ var windowHeight; //The height of the browser window
 var listOfCubes = new LinkedList(); //Stores the objects in the world
 var parsedData;
 
+// largest datapoints for calcuating graph midpoint and camera zoom
+var largestEntry;
+var largestX;
+var largestY;
+var largestZ;
 
+var graphCenter;
+
+// Object for OrbitControls.js for orbital camera controls
+var controls;
 
 /*
 Listens and adapts the aspect ratio and size of the canvas
@@ -20,6 +28,7 @@ resized.
 */
 function listenForWindowResize()
 {
+
   window.addEventListener('resize', function()
   {
     windowWidth = window.innerWidth;
@@ -44,6 +53,12 @@ function update()
     listOfCubes.elementAt(i).rotation.x += 0.01;
     listOfCubes.elementAt(i).rotation.y += 0.005;
   }
+
+  //requestAnimationFrame( animate );
+
+  // required if controls.enableDamping or controls.autoRotate are set to true
+  if (controls)
+    controls.update();
 }
 
 //Draw game objects to the scene
@@ -74,7 +89,7 @@ function build3DSpace()
   //Initialize camera, scene, and renderer
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000);
-  
+
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   //Add the renderer to the html page
@@ -88,24 +103,42 @@ function build3DSpace()
   var cube;
 
 
-  //console.log(parsedData);
+  largestX = 0;
+  largestY = 0;
+  largestZ = 0;
+  largestEntry = 0;
 
-
-  var largestEntry = 0;
+  // Loop through all the parsed data to generate a visualization
   for(var i = 0; i < parsedData.length; i++)
   {
+    // Create a new cube object
     boxGeometry = new THREE.BoxGeometry(0.3, 0.3, 0.3);
-    material = new THREE.MeshBasicMaterial({color: (0xffffff / parsedData[i][0]), wireframe: false});
+    material = new THREE.MeshBasicMaterial({color: (0xffffff), wireframe: false});
     cube = new THREE.Mesh(boxGeometry, material);
+
+    // Position cube based on first 3 columns of input data
     cube.position.x = parsedData[i][0];
-    //console.log(parsedData[i][0]);
     cube.position.y = parsedData[i][1];
-    //console.log(parsedData[i][1]);
     cube.position.z = parsedData[i][2];
-    //console.log(parsedData[i][2]);
+
+    // Find the X, Y, and Z value ceilings in the data.
+    if (parsedData[i][0] > largestX){
+      largestX = parsedData[i][0];
+    }
+    if (parsedData[i][1] > largestY){
+      largestY = parsedData[i][1];
+    }
+    if (parsedData[i][2] > largestZ){
+      largestZ = parsedData[i][2];
+    }
+
+    // Create Vector3 object representing the graph center point for camera
+    // target
+    graphCenter = new THREE.Vector3(largestX/2.0, largestY/2.0, largestZ/2.0);
 
 
-    // Find largest entry to adjust camera position
+    // Find largest entry across all axes to adjust initial camera zoom
+    // (Probably doesn't need to search entire data set all over again).
     for(j = 0; j < 3; j++){
     	if (parsedData[i][j] > largestEntry){
     		largestEntry = parsedData[i][j];
@@ -113,21 +146,42 @@ function build3DSpace()
     }
 
     scene.add(cube);
-
-
     listOfCubes.add(cube);
   }
-  console.log(largestEntry);
-  //Move camera back so that you are not inside the first cube
-  camera.position.x = largestEntry / 2.0;
-  camera.position.y = largestEntry / 2.0;
-  camera.position.z = largestEntry / 0.7; //magic number for "zoom"
 
+  // Orbital camera control initialization
+  var controls = new THREE.OrbitControls( camera );
+  camera.position.set(graphCenter.x, graphCenter.y, largestEntry / 0.6);
+  controls.target = graphCenter;
+  controls.enableDamping;
+  controls.autoRotate = true; // This isn't working for some reason.
 
+  // controls.update() must be called after any manual changes to the camera's
+  // transform
+
+  setCubeColors();
   drawFPSstats();
   //GameLoop must be called last after everything to ensure that
   //everything is rendered
   GameLoop();
+}
+
+//Loops through the cube list and sets RGB color values based on
+//()(XYZ Magnitude)/largestEntry) * 255
+function setCubeColors()
+{
+  for(var i = 0; i < listOfCubes.size(); i++)
+  {
+    // Truncating the first and last 16 of each value avoids a querk in
+    // toString(16) where it doesn't return leading zeros.
+    var r = 16 + Math.round((listOfCubes.elementAt(i).position.x / largestX) * 239);
+    var g = 16 + Math.round((listOfCubes.elementAt(i).position.y / largestY) * 239);
+    var b = 16 + Math.round((listOfCubes.elementAt(i).position.z / largestZ) * 239);
+
+    var colorHex = parseInt(r.toString(16) + g.toString(16) + b.toString(16), 16);
+
+    listOfCubes.elementAt(i).material.color.setHex( colorHex );
+  }
 }
 
 //This draws the fps and various stats to the page.

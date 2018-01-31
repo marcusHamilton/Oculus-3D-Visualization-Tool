fa4d309b1c880884bff0f6e38cb367ccf0c4d94d
Owen <ogs091@mail.usask.ca>
2018-01-30 22:13:54 -0600

    testing

--- test/firebaseTests.js
+++ test/firebaseTests.js
@@ -1,23 +1,47 @@
-var FirebaseServer = require('firebase-server');
-var Firebase = require('firebase');
+var firebaseServer = require('firebase-server');
+var firebase = require('firebase');
 
-fbServer = new FirebaseServer(5000, 'localhost.firebaseio.test', {
+//Create Firebase server
+fbServer = new firebaseServer(5000, 'localhost.firebaseio.test', {
     workers: {
         tester: "Owen",
         dev: "wynston"
     }
 });
 
+/*
 fbServer.getValue().then(function(result) {
   console.log(result);}, function(err) {
   console.log('it fucked up'); // Error: "It broke"
 });
+*/
 
+//fake api key and url of the locally hosted firebase database
 const config = {
    apiKey: '12345',
    databaseURL: 'ws://localhost.firebaseio.test:5000'
  }
-Firebase.initializeApp(config, 'TestingEnvironment');
+
+var app = firebase.initializeApp(config, 'TestingEnvironment');
+console.log('local Firebase Initialized');
+
+var db = app.database();
+
+//write data to the test database
+function writeUserData(data) {
+  db.ref('/').set({
+    item: data
+  });
+}
+writeUserData('farts and ass');
+
+//read all data from the database and print it
+return db.ref('/').once('value').then(function(snapshot) {
+  var data = (snapshot.val()) || 'Anonymous';
+  console.log(data);
+});
+
+
 
 after(function(){
   fbServer.close(console.log('server closed'));

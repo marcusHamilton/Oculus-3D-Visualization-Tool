f6f3fa1bb7ba1364c049c670e568070850d8fcdd
Wynston <wramsay@hotmail.ca>
2018-02-04 04:57:49 -0600

    Firebase hosting integrated
    -had to partially re-structure the code base to accomodate for firebase
    -view, app.js, and routing files now reside in the functions directory

--- functions/app.js
+++ functions/app.js
@@ -1,8 +1,11 @@
+var functions = require('firebase-functions');
+var firebase = require('firebase-admin');
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var engines = require('consolidate');
 
 var index = require('./routes/index');
 var localLoad = require('./routes/localLoad');
@@ -14,8 +17,14 @@ var VRWorld = require('./routes/VRWorld');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.engine('ejs', engines.handlebars);
+app.set('views', './views');
 app.set('view engine', 'ejs');
+
+// Firebase set-up
+var firebaseApp = firebase.initializeApp(
+  functions.config().firebase
+);
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -48,4 +57,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+exports.app = functions.https.onRequest(app);
+//module.exports = app;

13ba6fd473451bd76b006d10ba71ff3834cb4010
Owen <ogs091@mail.usask.ca>
2018-02-04 20:44:00 -0600

    went back to using different test firebase db

--- functions/test/firebaseTests.js
+++ functions/test/firebaseTests.js
@@ -1,13 +1,13 @@
-// var firebaseServer = require('firebase-server');
+var firebaseServer = require('firebase-server');
 var firebase = require('firebase');
 
 //Create Firebase server
-// fbServer = new firebaseServer(5000, 'localhost.firebaseio.test', {
-//     workers: {
-//         tester: "Owen",
-//         dev: "wynston"
-//     }
-// });
+fbServer = new firebaseServer(5000, 'localhost.firebaseio.test', {
+    workers: {
+        tester: "Owen",
+        dev: "wynston"
+    }
+});
 
 /*
 fbServer.getValue().then(function(result) {
@@ -16,27 +16,16 @@ fbServer.getValue().then(function(result) {
 });
 */
 
-// //fake api key and url of the locally hosted firebase database
-// const config = {
-//    apiKey: 'AIzaSyBqX2igua_Vqc3QMh9vESrIWwv3jjY9AhU',
-//    databaseURL: 'localhost:5000'
-//  }
+//fake api key and url of the locally hosted firebase database
+const config = {
+   apiKey: '12345',
+   databaseURL: 'ws://localhost.firebaseio.test:5000'
+ }
 
-// // Set the configuration for your app
-var config = {
-  apiKey: "AIzaSyBqX2igua_Vqc3QMh9vESrIWwv3jjY9AhU",
-  authDomain: "oculus-3d-visualization-c5687.firebaseapp.com",
-  databaseURL: "https://oculus-3d-visualization-c5687.firebaseio.com",
-  projectId: "oculus-3d-visualization-c5687",
-  storageBucket: "oculus-3d-visualization-c5687.appspot.com",
-  messagingSenderId: "483800110325"
-};
-firebase.initializeApp(config);
+var app = firebase.initializeApp(config, 'TestingEnvironment');
+console.log('local Firebase Initialized');
 
-console.log('Connected to firebase server');
-
-// Get a reference to the database service
-var db = firebase.database();
+var db = app.database();
 
 //write data to the test database
 function writeUserData(data) {
@@ -44,12 +33,16 @@ function writeUserData(data) {
     item: data
   });
 }
-console.log('writing to database');
-writeUserData('pls dont end up in production db');
+writeUserData('farts and ass');
 
 //read all data from the database and print it
-console.log('reading all data...');
 return db.ref('/').once('value').then(function(snapshot) {
   var data = (snapshot.val()) || 'Anonymous';
   console.log(data);
+});
+
+
+
+after(function(){
+  fbServer.close(console.log('server closed'));
 });

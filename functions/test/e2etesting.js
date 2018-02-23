b464af7a7317cdce0e40aff6990efc6442132902
hong <hox294@mail.usask.ca>
2018-02-23 01:35:57 -0600

    testing script update

--- functions/test/e2etesting.js
+++ functions/test/e2etesting.js
@@ -1,63 +1,46 @@
 /**
  * Created by homeyxue on 2018-02-03.
  */
+
+//selenium web driver require for all test suite
+var webdriver = require('selenium-webdriver'),
+    {describe, it, before, after} = require{'selenium-webdriver/testing'};
+    By = webdriver.By,
+        until = webdriver.until;
+
+    var driver;
+
+/**
+ * If we are using Firefox for testing, we need to set up
+ * Firefox browser option because our team use Firefox developer edition, not the default Firefox
+ */
+var firefox = require('selenium-webdriver/firefox');
+
+var profile = new firefox.Profile( '/Users/homeyxue/Library/Application Support/Firefox/Profiles/8htp4yu0.dev-edition-default');
+var firefoxOptions = new firefoxOptions().setProfile(profile);
+
 describe("Home page test Suite", function(){
-    browser.ignoreSynchronization = true;   //if not written by angluar
-    browser.get("https://3dvisualizationtool.ml");
 
+    this.timeout(50000);    //50 second time out for waiting
     /**
-     * browser set up and selenium web-driver buid
+     * web driver for each test case
      */
-    var webdriver = require('selenium-webdriver')
-    var chrome = require('selenium-webdriver/chrome');
-    var firefox = require('selenium-webdriver/firefox');
+    beforeEach(function(){
 
-    var profile = new firefox.Profile( '/Users/homeyxue/Library/Application Support/Firefox/Profiles/8htp4yu0.dev-edition-default');
-    var firefoxOptions = new firefoxOptions().setProfile(profile);
-
-    var profile1 = new chrome.Profile('/Users/homeyxue/Library/Application Support/Google/Chrome/Default');
-    var chromeOptions = new chromeOptions().setProfile(profile1);
-
-    var driver = new webdriver.Builder()
-        .forBrowser('firefox')
-        .setFirefoxOptions(firefoxOptions)
-        .setChromeOptions(chromeOptions)
-        .build();
-
-    /**
-     * test suite setup
-     */
-    before(function(){
+        driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .build();
 
         driver.get("https://3dvisualizationtool.ml");
 
-        // a promise is returned while ‘click’ action is registered in ‘driver’ object
-        //return driver.findElement(webdriver.By.id(loadLocal)).click();  /** id=loadLocal have NOT been added into html yet*/
     });
 
     /**
-     * web-driver close after test suite finish
-     */
-    after(function(){
-
-        return driver.quit();
-
-    });
-
-    beforeEach(function(){
-
-        // do something before test case execution
-        // no matter if there are failed cases
-
-    });
-
-    /**
-     * test suite teardown repeat(not use yet)
+     * close web driver after each test case
      */
     afterEach(function(){
 
-        // do something after test case execution is finished
-        // no matter if there are failed cases
+        return driver.quit(s);
 
     });
 
@@ -82,7 +65,6 @@ describe("Home page test Suite", function(){
         var my_title = "3D Visualization Tool";
         driver.getTitle().then(function ( title ) {
             expect(title).equals( my_title );
-            console.log(title)      // assertions: "3D Visualization Tool" should be printed
         });
     });
 
@@ -118,92 +100,42 @@ describe("Home page test Suite", function(){
     })
 });
 
-describe("Load Local page test Suite", function(){
-    browser.ignoreSynchronization = true;   //if not written by angluar
-    browser.get("https://3dvisualizationtool.ml");
-
+describe("Dashboard page test Suite", function(){
+    this.timeout(50000);    //50 second time out for waiting
     /**
-     * browser set up and selenium web-driver buid
+     * web driver for each test case
      */
-    var webdriver = require('selenium-webdriver')
-    var chrome = require('selenium-webdriver/chrome');
-    var firefox = require('selenium-webdriver/firefox');
-
-    var profile = new firefox.Profile( '/Users/homeyxue/Library/Application Support/Firefox/Profiles/8htp4yu0.dev-edition-default');
-    var firefoxOptions = new firefoxOptions().setProfile(profile);
-
-    var profile1 = new chrome.Profile('/Users/homeyxue/Library/Application Support/Google/Chrome/Default');
-    var chromeOptions = new chromeOptions().setProfile(profile1);
-
-    var driver = new webdriver.Builder()
-        .forBrowser('firefox')
-        .setFirefoxOptions(firefoxOptions)
-        .setChromeOptions(chromeOptions)
-        .build();
-
-    /**
-     * test suite setup
-     */
-    before(function(){
-
-        driver.get("https://3dvisualizationtool.ml");
-
-        // a promise is returned while ‘click’ action is registered in ‘driver’ object
-        return driver.findElement(webdriver.By.id(loadLocal)).click();  /** id=loadLocal have NOT been added into html yet*/
-    });
-
-    /**
-     * web-driver close after test suite finish
-     */
-    after(function(){
-
-        return driver.quit();
-
-    });
-
     beforeEach(function(){
 
-        // do something before test case execution
-        // no matter if there are failed cases
+        driver = new webdriver.Builder()
+            .forBrowser('chrome')
+            .build();
+
+        driver.get("https://3dvisualizationtool.ml");
+        // a promise is returned while ‘click’ action is registered in ‘driver’ object
+        return driver.findElement(webdriver.By.className('btn btn-lg btn-default')).click();  /** Dashboard button should click*/
 
     });
 
     /**
-     * test suite teardown repeat(not use yet)
+     * close web driver after each test case
      */
     afterEach(function(){
 
-        // do something after test case execution is finished
-        // no matter if there are failed cases
+        return driver.quit(s);
 
     });
 
-    it("Timeout", function(){
-
-        var promise;
-        promise = new Promise(function(resolve, reject){
-            setTimeout(function(){
-
-                console.log("Timeout");
-                resolve();
-
-            }, 3000);
-
-        });
-        // mocha will wait for the promise to be resolved before exiting
-        return promise;
-    });
-
-    it("localLoad button should be active", function(){
+    it("dashboard button should be active", function(){
 
         var my_classType = 'active';
-        var my_href = "/localLoad";
+        var my_href = "/dashboard";
 
         var button = element.all(by.className(my_classType));
         expect(button.href).toEqual(my_href);
     });
 
-    it("localLoad page body should display", function() {
+    it("dashboard page body should display", function() {
 
         var body_1 = element.all(by.className('site-wapper'));
         var body_2 = element.all(by.className('site-wapper-inner'));
@@ -214,7 +146,7 @@ describe("Load Local page test Suite", function(){
         expect(body_3.isDisplayed()).toEqual([true]);
     })
 
-    it("localLoad page title & nav should display", function() {
+    it("dashboard page title & nav should display", function() {
 
         var top = element.all(by.className('container inner'));
         var title = element.all(by.className('masthead-brand'));
@@ -225,7 +157,7 @@ describe("Load Local page test Suite", function(){
         expect(nav.isDisplayed()).toEqual([true]);
     })
 
-    it("load local page should load file", function() {
+    it("should load file", function() {
         /**
          * TODO: click load local(href = /localLoad); get load file; check data correctness
          */

96f1c0ad29b50d600488503e7f79857b7c3867ad
Owen <ogs091@mail.usask.ca>
2018-03-13 21:06:04 -0600

    test for adding parsed data to the scene file

--- functions/test/newWorldTests.js
+++ functions/test/newWorldTests.js
@@ -9,14 +9,53 @@
   4. Test newWorld.js functions in Mocha to verify that the results match the results file.
 */
 
+// var THREE = require("three-js")();
 const assert = require('chai').assert;
+const chai = require('chai');
+var expect = chai.expect;
 const mocha = require('mocha');
+const sinon = require('sinon');
 
 var newWorld = require('../../public/javascripts/newWorld.js');
+var THREE = newWorld.THREE;
+
+var writeWorldStub = sinon.stub();
+// console.log('newWorld is: ' + Object.keys(newWorld));
+
+function writeWorld(json){
+  return null;
+}
+
+function reloadWorlds(){
+  return null;
+}
 
 describe('testing newWorld.js', function(){
-  it('should print testing', function(){
-    console.log('testing');
 
-  });
+  describe('verify parsed data is loaded into scene correctly', function(){
+
+    before(function(){
+      var parsedData = ['index', "x", "y", "z",1,1,1,1,2,2,2,2,3,3,3,3,]
+      newWorld.setParsedData(parsedData);
+      newWorld.setXAxisIndex(1);
+      newWorld.setYAxisIndex(2);
+      newWorld.setZAxisIndex(3);
+      newWorld.setSceneForTesting();
+
+      sceneUserData = [1,2,3,'index', "x", "y", "z",1,1,1,1,2,2,2,2,3,3,3,3,]
+    });
+
+    it('should be correct in scene.userData', function(){
+      newWorld.addParsedDataToScene();
+      expect(newWorld.getScene().userData).to.deep.equal(sceneUserData)
+
+    });
+  })
+
 });
+
+
+/*
+parsed data: index, "x", "y", "z",1,1,1,1,2,2,2,2,3,3,3,3,
+scene.userData: 1,2,3,index, "x", "y", "z",1,1,1,1,2,2,2,2,3,3,3,3,
+*/

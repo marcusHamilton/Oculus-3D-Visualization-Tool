87950d1690891c1df4a2d73445c868fecfc2d11f
Marcus Hamilton <mah985@mail.usask.ca>
2018-03-16 13:02:29 -0600

    FIx bug when both error messages display

--- public/javascripts/dashboardManager.js
+++ public/javascripts/dashboardManager.js
@@ -52,10 +52,12 @@ function addUser()
     var reg = new RegExp("^[a-z0-9](\.?[a-z0-9]){5,}@gmail\.com$");
     if(document.getElementById('addEmail').value == "")
     {
+        document.getElementById('badEmailMessage').style="display:none";
         document.getElementById('noEmailMessage').style="display:block";
     }
     else if(!reg.test(document.getElementById('addEmail').value))
     {
+        document.getElementById('noEmailMessage').style="display:none";
         document.getElementById('badEmailMessage').style="display:block";
     }
     else
@@ -63,6 +65,8 @@ function addUser()
         var email = document.getElementById('addEmail').value;
         var worldID = document.getElementById('worldIDInputBox').value;
         console.log("Adding email: " + email + " to world: " + worldID);
+
+        //Do stuff with email and worldID here
 
         //Reset the modal box for more use
         $('#addUser-modal').modal('hide');

6b13604d5a8430425868fa521afe9f14359bbdea
unimatrixZero1 <d.eklund.mx3@gmail.com>
2018-03-18 15:27:41 -0600

    Raycaster experimentation: making it visible.

--- public/javascripts/pointSelection.js
+++ public/javascripts/pointSelection.js
@@ -118,7 +118,9 @@ function pointSelectionUpdate() {
     setPointScale(mousedOverPoint, plotPointSizeCoeff * Math.max(plotInitSizeX, plotInitSizeY, plotInitSizeZ));
   }
   scene.remove ( arrow );
-  arrow = new THREE.ArrowHelper( pointSelectionRaycasterR.direction, pointSelectionRaycasterR.origin, 100, 0xff00ff );
+  if (pointSelectionRaycasterR) {
+    arrow = new THREE.ArrowHelper(pointSelectionRaycasterR.direction, pointSelectionRaycasterR.origin, 100, 0xff00ff);
+  }
   scene.add( arrow );
 }
 

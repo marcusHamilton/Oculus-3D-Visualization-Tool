f5e524466c9dc03312a06cfb88ddfcd2de6faa96
unimatrixZero1 <d.eklund.mx3@gmail.com>
2018-03-20 21:26:31 -0600

    Added dataset rotation.

--- public/javascripts/movementControlsVR.js
+++ public/javascripts/movementControlsVR.js
@@ -26,32 +26,10 @@ function updateMovementControls(){
   }
   if (movementControllerR){
     if (datasetAndAxisLabelGroup) {
-      //datasetAndAxisLabelGroup.rotateY(movementControllerR.getAxis(0) * movementSpeedCoeff * -1);
-
-      rotateAboutPoint(datasetAndAxisLabelGroup, new THREE.Vector3(5,0,5), new THREE.Vector3(0,1,0), movementControllerR.getAxis(0) * movementSpeedCoeff * -1, true);
+      datasetAndAxisLabelGroup.position = new THREE.Vector3(-5,0,-5);
+      datasetAndAxisLabelGroup.rotateY(movementControllerR.getAxis(0) * movementSpeedCoeff * -1);
+      datasetAndAxisLabelGroup.position = new THREE.Vector3(5,0,5);
     }
   }
 }
 
-// obj - your object (THREE.Object3D or derived)
-// point - the point of rotation (THREE.Vector3)
-// axis - the axis of rotation (normalized THREE.Vector3)
-// theta - radian value of rotation
-// pointIsWorld - boolean indicating the point is in world coordinates (default = false)
-function rotateAboutPoint(obj, point, axis, theta, pointIsWorld){
-  pointIsWorld = (pointIsWorld === undefined)? false : pointIsWorld;
-
-  if(pointIsWorld){
-    obj.parent.localToWorld(obj.position); // compensate for world coordinate
-  }
-
-  obj.position.sub(point); // remove the offset
-  obj.position.applyAxisAngle(axis, theta); // rotate the POSITION
-  obj.position.add(point); // re-add the offset
-
-  if(pointIsWorld){
-    obj.parent.worldToLocal(obj.position); // undo world coordinates compensation
-  }
-
-  obj.rotateOnAxis(axis, theta); // rotate the OBJECT
-}

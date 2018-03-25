e303fd0ca1b1e6c5c931db035ba3707ba454e689
Fahd Hussain <fahd.h12@gmail.com>
2018-03-25 17:36:22 -0600

    connectecd raycaster to aRightMesh

--- public/javascripts/pointSelection.js
+++ public/javascripts/pointSelection.js
@@ -100,13 +100,14 @@ function pointSelectionUpdate() {
   if (selectionControllerR) {
     var matrix = new THREE.Matrix4();
     //rig.add(selectionControllerR);
-    matrix.extractRotation( selectionControllerR.matrix );
+    matrix.extractRotation( aRightMesh.matrix );
     var direction = new THREE.Vector3( 0, 0, 1 );
     direction.applyMatrix4(matrix);
     var meshPosition = aRightMesh.getWorldPosition();
     //matrix.multiplyVector3( direction );
     direction.multiplyScalar(-1);
     //selectionControllerR.position = handControlR.position;
+
     pointSelectionRaycasterR.set(meshPosition, direction);
     //console.log("Controller Position: ");
     //console.log(handControlR.position);

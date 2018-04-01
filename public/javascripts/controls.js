424a7723f8394ca1f26013f90a5f59d76116eb0c
Fahd Hussain <fahd.h12@gmail.com>
2018-04-01 14:44:50 -0600

    made directionArrow.setDirection()

--- public/javascripts/controls.js
+++ public/javascripts/controls.js
@@ -311,7 +311,7 @@ function setListeners() {
         handControlR.addEventListener('thumbstick touch began', function (event) {
             //rig.add(directionArrow);
             camera.getWorldDirection(cameraDirection);
-            directionArrow.set(cameraDirection.normalize())
+            directionArrow.setDirection(cameraDirection.normalize());
             scene.add(directionArrow);
         });
         handControlR.addEventListener('thumbstick touch ended', function (event) {

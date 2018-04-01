0931386786f6f61e0b222ad53ab91eb702f16d45
Fahd Hussain <fahd.h12@gmail.com>
2018-04-01 17:04:14 -0600

    z at 0

--- public/javascripts/controls.js
+++ public/javascripts/controls.js
@@ -335,7 +335,7 @@ function setListeners() {
             arrowMesh.position = camera.getWorldPosition();
             arrowMesh.position.y = -1;
             arrowMesh.position.x = 1;
-            arrowMesh.position.z = -0.5;
+            arrowMesh.position.z = 0;
             rig.add(arrowMesh);
 
         });

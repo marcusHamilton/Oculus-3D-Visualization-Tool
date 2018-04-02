c933cc499c9a53d27150b5b74f43942994c56d1c
Nicholas Seaboyer (nas260) <nap.s@usask.ca>
2018-04-02 17:13:25 -0600

    fixes how selectionThreshold finds it's size

--- public/javascripts/pointSelection.js
+++ public/javascripts/pointSelection.js
@@ -11,7 +11,7 @@ var selectedPoints = []; //array containing the indices of every currently
 var hiddenPoints = [];
 
 var pointSelectionMouse = new THREE.Vector2();
-var selectionThreshold = 0.04 * datasetAndAxisLabelGroup.scale; //the distance the mouse has to be from a point
+var selectionThreshold = 0.04 * pointVars.plotPointSizeCoeff + 0.001; //the distance the mouse has to be from a point
 //in order for it to register as selectable
 var intersects;
 
@@ -166,7 +166,7 @@ function pointSelectionUpdate() {
       scene.add(raycasterLine);
     }
   }
-    selectionThreshold = 0.04 * datasetAndAxisLabelGroup.scale;
+    selectionThreshold = 0.04 * pointVars.plotPointSizeCoeff + 0.001;
 }
 
 /**

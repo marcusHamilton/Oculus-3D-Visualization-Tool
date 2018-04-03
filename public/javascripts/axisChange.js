c8a246bd9acb9d4724bd34f3e882ac195e63ff86
Unknown <scott_laskowski@live.com>
2018-04-02 20:23:27 -0600

    Blep

--- public/javascripts/axisChange.js
+++ public/javascripts/axisChange.js
@@ -43,7 +43,7 @@ function redrawDataSetVR() {
       console.log("Z-Axis: " + axisMenu.zAxis + "|VR selected: " + axisMenu.axesOptions.indexOf(selectedAxes.selectedZ));
 
       // console.log("Removing children")
-	  scene.remove(collabGroup);
+	 collabGroup.remove(scene.getObjectByName("DatasetAxisGroup"));
 
       /*while (scene.children.length > 0) {
         scene.remove(scene.children[0]);
